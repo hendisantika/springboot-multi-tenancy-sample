c18a15b6f7c604562df792fa398006c5ecca021d
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:44:00 +0700

    #21 - Add getEmployee in EmployeeController

--- src/main/java/com/hendisantika/springbootmultitenancysample/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootmultitenancysample/controller/EmployeeController.java
@@ -4,11 +4,13 @@ import com.hendisantika.springbootmultitenancysample.model.Employee;
 import com.hendisantika.springbootmultitenancysample.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +39,10 @@ public class EmployeeController {
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.createEmployee(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Employee> getEmployee(@PathVariable("id") String employeeId) {
+        return employeeService.getEmployee(employeeId);
     }
 }
