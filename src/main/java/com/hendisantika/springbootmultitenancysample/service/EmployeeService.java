17358f9f9ab8c81ec5346998b63f28a9179937df
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:30:08 +0700

    #12 - Add createEmployee in EmployeeService

--- src/main/java/com/hendisantika/springbootmultitenancysample/service/EmployeeService.java
+++ src/main/java/com/hendisantika/springbootmultitenancysample/service/EmployeeService.java
@@ -1,8 +1,14 @@
 package com.hendisantika.springbootmultitenancysample.service;
 
+import com.hendisantika.springbootmultitenancysample.model.Employee;
 import com.hendisantika.springbootmultitenancysample.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +23,17 @@ import org.springframework.stereotype.Service;
 public class EmployeeService implements ApplicationRunner {
 
     private final EmployeeRepository employeeRepository;
+
+    @PersistenceContext
+    public EntityManager entityManager;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Transactional
+    public Employee createEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
