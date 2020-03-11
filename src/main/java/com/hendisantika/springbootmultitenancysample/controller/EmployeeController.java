b5da470707f7f571f319ed363cced6c7ea23987b
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:43:41 +0700

    #20 - Add createEmployee in EmployeeController

--- src/main/java/com/hendisantika/springbootmultitenancysample/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/springbootmultitenancysample/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.hendisantika.springbootmultitenancysample.model.Employee;
 import com.hendisantika.springbootmultitenancysample.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -30,5 +32,10 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> listEmployees() {
         return employeeService.listEmployees();
+    }
+
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
     }
 }
