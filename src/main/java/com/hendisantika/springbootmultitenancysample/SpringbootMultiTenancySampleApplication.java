50c974bca98e1d43e800957b95dfd23ba2fdbbae
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:22:57 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmultitenancysample/SpringbootMultiTenancySampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultitenancysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMultiTenancySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMultiTenancySampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootmultitenancysample/SpringbootMultiTenancySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootmultitenancysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMultiTenancySampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
