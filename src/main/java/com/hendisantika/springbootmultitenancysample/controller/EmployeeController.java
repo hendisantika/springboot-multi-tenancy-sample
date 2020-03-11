3f8e417b3396f2ce3296ba9fdd586bc54cf645f7
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:43:09 +0700

    #18 - Add EmployeeController

---
+++ src/main/java/com/hendisantika/springbootmultitenancysample/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootmultitenancysample.controller;
+
+import com.hendisantika.springbootmultitenancysample.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multi-tenancy-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/20
+ * Time: 06.42
+ */
+@RestController("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
