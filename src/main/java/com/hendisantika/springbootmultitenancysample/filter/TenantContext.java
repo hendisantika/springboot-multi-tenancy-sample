26ee8ec9b6e36f187a4f38d0518f0fdab8f8979b
Hendi Santika <hendisantika@gmail.com>
2020-03-12 06:28:55 +0700

    #10 - Add TenantContext

---
+++ src/main/java/com/hendisantika/springbootmultitenancysample/filter/TenantContext.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootmultitenancysample.filter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-multi-tenancy-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/03/20
+ * Time: 06.28
+ */
+public class TenantContext {
+    private static ThreadLocal<String> currentTenant = new ThreadLocal<>();
+
+    public static String getCurrentTenant() {
+        return currentTenant.get();
+    }
+
+    public static void setCurrentTenant(String tenant) {
+        currentTenant.set(tenant);
+    }
+
+    public static void clear() {
+        currentTenant.set(null);
+    }
+}
